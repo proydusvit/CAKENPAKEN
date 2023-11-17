489286e36aeb62860d83397ffae81c76be322b37
Vlad Shestavetkyi <95980041+proydusvit@users.noreply.github.com>
2023-11-17 19:39:04 +0200

    add

--- src/blocks/Gallery/Category.jsx
+++ src/blocks/Gallery/Category.jsx
@@ -2,39 +2,11 @@ import styles from "./Category.module.scss";
 import { listCategory } from "./galleryList";
 import Link from "next/link";
 import { useTranslation } from "next-i18next";
-import Loading from "pages/loading";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 const Category = () => {
   const { t } = useTranslation("gallery");
-  const router = useRouter();
-
-  const [loading, setLoading] = useState(false);
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    setLoading(true); // Встановлюємо стан "loading" на true перед початком затримки
-    setTimeout(() => {
-      router.push(e.target.href);
-      setLoading(false); // Встановлюємо стан "loading" на false після завершення затримки
-    }, 3000); // Затримка в 4000 мс (4 секунди)
-  };
-
-  useEffect(() => {
-    const handleComplete = () => {
-      setLoading(false); // Встановлюємо стан "loading" на false, якщо перехід відбувся поза допомогою затримки
-    };
-
-    router.events.on("routeChangeComplete", handleComplete);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleComplete);
-    };
-  }, []);
 
   return (
     <>
-      {loading && <Loading />}
       <section className={styles.section}>
         <h1 className={styles.title}> {t("gallery")}</h1>
 
@@ -43,7 +15,6 @@ const Category = () => {
             const imgCatg = item.img;
             return (
               <Link
-                onClick={handleClick}
                 key={item.id}
                 href={item.link}
                 className={styles.links}
