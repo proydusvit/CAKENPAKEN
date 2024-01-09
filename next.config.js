dc4232c849fcb40b7c552ea4ad0d9aee8d8a4f14
Vlad Shestavetkyi <95980041+proydusvit@users.noreply.github.com>
2024-01-09 14:52:44 +0200

    add

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const { i18n } = require('./next-i18next.config');
 
 module.exports = {
   i18n,
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   images: {
     domains: ['res.cloudinary.com'],
     // domains: ["localhost"], // Add other domains if needed
--- src/pages/robots.js
+++ src/pages/robots.js
@@ -1,11 +1,26 @@
-export default function robots() {
+// Для англійської мови (en)
+function robotsEn() {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/private/',
     },
+    host: 'https://cakenpaken-6n28-git-main-proydusvit.vercel.app/en', // змініть шлях відповідно до англійської версії вашого сайту
     sitemap:
       'https://cakenpaken-6n28-git-main-proydusvit.vercel.app/sitemap.xml',
   };
 }
+
+// Для української мови (uk)
+function robotsUk() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    host: 'https://cakenpaken-6n28-git-main-proydusvit.vercel.app/uk', // змініть шлях відповідно до української версії вашого сайту
+    sitemap:
+      'https://cakenpaken-6n28-git-main-proydusvit.vercel.app/uk/sitemap.xml',
+  };
+}
+export { robotsUk, robotsEn };
